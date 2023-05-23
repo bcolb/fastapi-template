9d4868509fcaac346ad97e703c3afb0ac3e15fdd
Brice Colbert <brice.tech@gmail.com>
2023-05-22 23:22:58 -0700

    simple postgres integration

---
+++ app/config.py
@@ -0,0 +1,8 @@
+# app/config.py
+
+from pydantic import BaseSettings, Field
+
+class Settings(BaseSettings):
+    db_url: str = Field(..., env='DATABASE_URL')
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.db import database, User
+
+app = FastAPI(title="FastAPI Template")
 
 @app.get("/")
+async def read_root():
+    ''' Gives a list of users'''
+    return await User.objects.all()
+
+@app.get("/hello")
 def hello():
+    ''' Returns the hello message in json'''
     return {"message": "hello"}
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # Make a dummy user entry for now
+    await User.objects.get_or_create(email="dev@dev.org")
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
+        
